f099608c934daa1fc111ff9a97265dcd4f63e8ff
Ahn Junghyo <ahnjunghyo@Ahnui-MacBookAir.local>
2022-03-09 15:41:17 +0900

    Updates

---
+++ precache-manifest.9a48cdfde36306bd4635ff07f98224d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d81643401959513638e",
+    "url": "/react-for-beginners2/static/js/main.3d816434.chunk.js"
+  },
+  {
+    "revision": "705c5c6b941de762eec7",
+    "url": "/react-for-beginners2/static/js/1.705c5c6b.chunk.js"
+  },
+  {
+    "revision": "ba4602e8aa760b90761d",
+    "url": "/react-for-beginners2/static/js/runtime~main.ba4602e8.js"
+  },
+  {
+    "revision": "b68515de29c3b993e66114e6b966508c",
+    "url": "/react-for-beginners2/index.html"
+  }
+];
